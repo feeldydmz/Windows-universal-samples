1995972fb3822dc34f2593a8ca1970d6d5569e95
Cho Yong wook <feeldyd@mz.co.kr>
2019-10-30 16:38:07 +0900

    referesh 토큰 기능 추가 누락 파일 추가

---
+++ Megazone.HyperSubtitleEditor.Presentation/ViewModel/Data/AuthorizationInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Megazone.Cloud.Media.ServiceInterface.Model;
+
+namespace Megazone.HyperSubtitleEditor.Presentation.ViewModel.Data
+{
+    public class AuthorizationInfo : Authorization
+    {
+        public AuthorizationInfo(string accessToken, string refreshToken, string expires, DateTime expiresTime)
+         :base (accessToken, refreshToken, expires)
+        {
+            /*Authorization = authorization;
+            ExpiresTime = expireTime;*/
+            ExpiresTime = expiresTime;
+        }
+
+//        public AuthorizationInfo(string accessToken, string refreshToken, string expires, string expiresTime)
+//            : base(accessToken, refreshToken, expires)
+//        {
+//            /*Authorization = authorization;
+//            ExpiresTime = expireTime;*/
+//            ExpiresTime = DateTime.Parse(expireTime);
+//        }
+
+        //        public Authorization Authorization { get; private set; }
+        public DateTime ExpiresTime { get; set; }
+    }
+}
